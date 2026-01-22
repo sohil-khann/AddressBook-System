de6a841dd8366a92536f62d31d8c5dede5a0c5be
sohil-khann <khaa40408@gmail.com>
2026-01-22 15:18:31 +0530

    [Sohil khan] Add. Welcome message

---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace AddressBookSystem;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Address Book System!");
+    }
+}
