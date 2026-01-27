ca52fcd8a15664d39da2d02c9963e066572b53e9
sohil-khann <khaa40408@gmail.com>
2026-01-27 12:35:21 +0530

    [Sohil khan] Update. Contact poco class

--- AddressBookSystem/Contact.cs
+++ AddressBookSystem/Contact.cs
@@ -8,10 +8,10 @@ namespace AddressBookSystem
     public string Address{get; set;}
     public string City{get; set;}
     public string State{get; set;}
-    public string Zip{get; set;}
-    public string PhoneNumber{get; set;}
+    public int Zip{get; set;}
+    public int PhoneNumber{get; set;}
     public string Email{get; set;}
-    public Contact(string firstname,string lastname,string address,string city,string state,string zip,string phoneNumber,string email)//parameterized constructor
+    public Contact(string firstname,string lastname,string address,string city,string state,int zip,int phoneNumber,string email)//parameterized constructor
         {
             this.FirstName=firstname;
             this.LastName=lastname;  
@@ -27,5 +27,48 @@ namespace AddressBookSystem
             return $"FirstName: {FirstName}, LastName: {LastName}, Address: {Address}, City: {City}, State: {State}, Zip: {Zip}, PhoneNumber: {PhoneNumber}, Email: {Email}";
         }
 
-}
+        public override bool Equals(object obj)
+        {
+            // if the object is not a Contact, they can't be equal
+            if (obj == null || !(obj is Contact))
+                return false;
+
+            // cast to Contact so we can look at its fields
+            Contact other = (Contact)obj;
+
+            // compare first names (ignore upper/lower case)
+            if (FirstName == null || other.FirstName == null)
+            {
+                if (FirstName != other.FirstName)
+                    return false;
+            }
+            else
+            {
+                if (!FirstName.ToLower().Equals(other.FirstName.ToLower()))
+                    return false;
+            }
+
+            // compare last names (ignore upper/lower case)
+            if (LastName == null || other.LastName == null)
+            {
+                if (LastName != other.LastName)
+                    return false;
+            }
+            else
+            {
+                if (!LastName.ToLower().Equals(other.LastName.ToLower()))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            // alternative: use HashCode.Combine for a concise, case-insensitive hash
+            return HashCode.Combine(
+                FirstName?.ToLowerInvariant(),
+                LastName?.ToLowerInvariant());
+        }
+    }
 }
