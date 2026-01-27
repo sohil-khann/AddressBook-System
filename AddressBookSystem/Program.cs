ae3394fda1e651c309c4c219c9f0823709dd79f7
sohil-khann <khaa40408@gmail.com>
2026-01-27 11:46:02 +0530

    [Sohil khan] Update. Program.cs

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -1,9 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 namespace AddressBookSystem;
+
 public class Program
 {
     public static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Address Book System!");
+
+        Console.WriteLine("Add Contact to Address Book:");
+        ManageContacts manageContacts = new ManageContacts();
+        manageContacts.Addcontact();
+        manageContacts.EditContact();
+        manageContacts.DeleteContact();
+
+        Console.ReadLine();
     }
 }
