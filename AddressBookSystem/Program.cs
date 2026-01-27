1195d95a1c98f700a65d85c3c74fbc61196e0f47
sohil-khann <khaa40408@gmail.com>
2026-01-27 11:54:31 +0530

    [Sohil khan] Update. Program.cs

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -7,12 +7,43 @@ public class Program
     {
         Console.WriteLine("Welcome to Address Book System!");
 
-        Console.WriteLine("Add Contact to Address Book:");
         ManageContacts manageContacts = new ManageContacts();
-        manageContacts.Addcontact();
-        manageContacts.EditContact();
-        manageContacts.DeleteContact();
+        bool exit = false;
 
-        Console.ReadLine();
+        while (!exit)
+        {
+            Console.WriteLine("\n--- Address Book Menu ---");
+            Console.WriteLine("1. Add Contact");
+            Console.WriteLine("2. Edit Contact");
+            Console.WriteLine("3. Delete Contact");
+            Console.WriteLine("4. Display All Contacts");
+            Console.WriteLine("5. Exit");
+            Console.Write("Choose an option: ");
+            
+            string choice = Console.ReadLine() ?? "";
+
+            switch (choice)
+            {
+                case "1":
+                    manageContacts.Addcontact();
+                    break;
+                case "2":
+                    manageContacts.EditContact();
+                    break;
+                case "3":
+                    manageContacts.DeleteContact();
+                    break;
+                case "4":
+                    manageContacts.DisplayContacts();
+                    break;
+                case "5":
+                    exit = true;
+                    Console.WriteLine("Exiting Address Book System. Goodbye!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
     }
 }
